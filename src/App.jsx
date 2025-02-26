7551938537f75a15bd584dcefc6f55e034f76ae7
Codewizard-18 <jaswanthm811@gmail.com>
2025-02-26 11:43:55 +0530

    made header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-
+import React from "react";
+import Header from "./components/head/head";
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <span>
+      <Header/>
+    </span>
   );
 }
 
